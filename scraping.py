d28bf523390a4618893527d42ec3776fc4dcc84f
Adrian Bui <bqthinh1999@yahoo.com>
2020-11-16 22:03:50 -0600

    get started

--- scraping.py
+++ scraping.py
@@ -1,1 +1,7 @@
-print("Hello world!")
+import requests
+from bs4 import BeautifulSoup
+
+page = requests.get("https://blog.ongig.com/diversity-and-inclusion/biased-language-examples/")
+soup = BeautifulSoup(page.content, 'html.parser')
+
+print(soup.find_all())
