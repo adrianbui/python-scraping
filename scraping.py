91a2094c39fd75ffa6e65bbaca34b945fc078f2f
Adrian Bui <bqthinh1999@yahoo.com>
2020-11-18 23:25:56 -0600

    make titles in place

--- scraping.py
+++ scraping.py
@@ -10,18 +10,24 @@ table = soup.find_all('table')[1]
 rowsInTable = table.find_all('tr')
 itemsInRow = list()
 
-for tableRow in rowsInTable: #loop through the rows in table
+
+#loop through the rows in table
+for tableRow in rowsInTable: 
     tableData = tableRow.find_all('td')
     row = [i.text for i in tableData]
     itemsInRow.append(row)
 
-
+#Enter titles for columns
 firstTitle = input("Enter the first title: ")
 secondTitle = input("Enter the second title: ")
 thirdTitle = input("Enter the third title: ")
 
 dataF = pd.DataFrame(itemsInRow, columns=[firstTitle, secondTitle, thirdTitle])
+dataF.set_index(firstTitle, inplace=True)
 dataF.set_index(secondTitle, inplace=True)
+dataF.set_index(thirdTitle, inplace=True)
 
+
+#Convert data to csv file
 fileName = input("Enter the filename: ")
 dataF.to_csv('{}.csv'.format(fileName))
